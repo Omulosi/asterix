75bbf88e2bd5b84d640fbb7f8544c9a2754df6ac
Omulosi <mulongojohnpaul@gmail.com>
2021-01-15 09:43:27 +0300

    Refactor Marker view to have a custom post method

--- api/asterix/apps/markers/rest_api/urls.py
+++ api/asterix/apps/markers/rest_api/urls.py
@@ -3,6 +3,6 @@ from django.urls import path
 from .views import marker_views
 
 urlpatterns = [
-    path('markers/', marker_views.MarkerListCreateAPIView.as_view(),
+    path('markers/', marker_views.MarkerList.as_view(),
          name='api-marker-list'),
 ]
--- api/asterix/apps/markers/rest_api/views/marker_views.py
+++ api/asterix/apps/markers/rest_api/views/marker_views.py
@@ -1,15 +1,26 @@
 
 """Markers view."""
 
-from rest_framework.generics import ListCreateAPIView, RetrieveUpdateDestroyAPIView
+from rest_framework.generics import ListAPIView
+from rest_framework.views import APIView
+from rest_framework.response import Response
 
 from asterix.apps.markers.models.marker import Marker
 from asterix.apps.markers.rest_api.serializers.marker import MarkerSerializer
 
 
-class MarkerListCreateAPIView(ListCreateAPIView):
+class MarkerList(APIView):
     """
     API view to retrieve list of markers or create new
     """
-    queryset = Marker.objects.all()
     serializer_class = MarkerSerializer
+
+    def get(self, request, version):
+        markers = Marker.objects.all()
+        data = MarkerSerializer(markers, many=True).data
+        return Response(data)
+
+    def post(self, request, version):
+        print("===============================");
+        print(request.data)
+        return Response("saved new features");
