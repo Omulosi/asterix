b11786924da45eff5c4c26a4e65f18e3859ae293
Omulosi <mulongojohnpaul@gmail.com>
2021-01-18 13:02:23 +0300

    Add login, signup and dashboard components: not fully functional yet

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,22 @@
-import './App.css';
-import React from 'react';
-import MapView from './components/MapView';
+import "./App.css";
+import React from "react";
+import { Switch, Route } from "react-router-dom";
+
+import Dashboard from "./components/dashboard/Dashboard";
+import SignIn from "./components/SignIn";
+import SignUp from "./components/SignUp";
 
 const App = () => {
-
   return (
-    <MapView />
+    <>
+      <Switch>
+        <Route exact path="/" component={SignIn} />
+        <Route path="/login" component={SignIn} />
+        <Route path="/signup" component={SignUp} />
+        <Route path="/dashboard" component={Dashboard} />
+      </Switch>
+    </>
   );
-}
+};
 
 export default App;
